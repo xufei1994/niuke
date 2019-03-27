c7abe4f17134c2a51d6a8004e60edd0da610a413
xufei1994 <878147704@qq.com>
2019-03-27 12:44:03 +0800

    更新了冒泡优化

--- src/main/java/com/basic/part1/Code_00_bubbleSort.java
+++ src/main/java/com/basic/part1/Code_00_bubbleSort.java
@@ -7,27 +7,34 @@ package com.basic.part1;/*
 */
 
 
+import java.util.Arrays;
+
 public class Code_00_bubbleSort {
     public static void main(String[] args) {
-        int [] array = new int[]{9,8,7,6,5,4,1,2,3};
+        int[] array=new int[]{8,9,3,2,1};
+        int [] array2 = new int[]{9,8,7,6,5,4,1,2,3};
         //bubbleSort(array);
+
         sort2(array);
+        sort2(array2);
         for (int i = 0; i <array.length ; i++) {
             System.out.print("  "+array[i]);
         }
+        System.out.println();
+        Arrays.stream(array2).forEach(value -> System.out.print(" "+value));
     }
     //改进  原理记录最后一位冒泡交换的下标，
     public static void sort2(int[] array){
         int m=array.length;
-        while (m > 1) {
-            int lastExchangeIndex = 1;
+        while (m !=0) {//这里的边界  要设置为0
+            int lastExchangeIndex = 0;
             for (int j = 0; j < array.length-1; j++) {
                 if (array[j] > array[j + 1]) {
                     swap(array, j, j + 1);
                     lastExchangeIndex = j;
-                }else {
-                    m = lastExchangeIndex;  //只要最后一位交换的下标不是0，就一直循环
                 }
+                    m = lastExchangeIndex;  //只要最后一位交换的下标不是0，就一直循环
+
             } // forj
         } //while
     }
